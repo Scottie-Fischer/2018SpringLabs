0bd9230e621ea5f3dbc57df43cdc00c1ec4465cc
sifische <sifische@ucsc.edu>
2018-05-10 15:41:30 -0700

    Testing Taking Input from User

--- lab4/Lab4.asm
+++ lab4/Lab4.asm
@@ -1,15 +1,34 @@
 .text
-
 main:
+	#Prompt Input Number
 	li $v0,4
-	la $a0,greeting
+	la $a0,greeting_prompt
 	syscall
 
+	#Take Input
+	li $v0, 5
+	syscall
 	
+	#Store Input
+	move $t0, $v0
+	
+	#Print Message
+	li $v0,4
+	la $a0,message
+	syscall
+	
+	#Print Input
+	li $v0,1
+	move $a0, $t0
+	syscall	
+									
 	li $v0, 10
 	syscall
+
 .data
-greeting:
-.asciiz "Hello World"
+greeting_prompt:
+.asciiz "Please Enter a Number:"
+message:
+.asciiz "\nYou entered: "
 
 	
