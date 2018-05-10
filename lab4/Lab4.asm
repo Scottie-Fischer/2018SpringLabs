a4e647eecb9432c0ff26da9aedbbe0fe1c865455
sifische <sifische@ucsc.edu>
2018-05-10 16:28:09 -0700

    Implemented Loop that prints every number up until User Inputed Number

--- lab4/Lab4.asm
+++ lab4/Lab4.asm
@@ -4,24 +4,41 @@ main:
 	li $v0,4
 	la $a0,greeting_prompt
 	syscall
-
+	
 	#Take Input
 	li $v0, 5
 	syscall
 	
 	#Store Input
-	move $t0, $v0
+	move $s0, $v0
 	
-	#Print Message
+#---------This is the loop---------#
+	#Creates Counter at $s1
+	li $s1, 0
+	#Creates Increment Variable
+	li $t1,1
+	
+	start_loop:
+		move $t0,$s1
+		
+		#Prints Counter Integer		
+		li $v0,1
+		move $a0,$t0
+		syscall
+		
+		#If counter equals user input jumps to end_loop
+		beq $s1,$s0,end_loop
+		
+		#If not equal adds 1 to counter and jumps to start_loop
+		add $s1,$s1,$t1
+		b start_loop
+	
+	end_loop:
 	li $v0,4
 	la $a0,message
 	syscall
 	
-	#Print Input
-	li $v0,1
-	move $a0, $t0
-	syscall	
-									
+	#Exit Program						
 	li $v0, 10
 	syscall
 
@@ -29,6 +46,6 @@ main:
 greeting_prompt:
 .asciiz "Please Enter a Number:"
 message:
-.asciiz "\nYou entered: "
+.asciiz "\nFinished Counting!"
 
 	
