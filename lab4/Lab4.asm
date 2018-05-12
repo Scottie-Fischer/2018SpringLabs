d3baf5fb5b530edd7bde9f8b76db9b3afef60517
sifische <sifische@ucsc.edu>
2018-05-12 10:40:50 -0700

    Implemented If-Else Block for checking Divisibility of number.

--- lab4/Lab4.asm
+++ lab4/Lab4.asm
@@ -1,5 +1,6 @@
 .text
 main:
+#---------This is the Prompt--------#
 	#Prompt Input Number
 	li $v0,4
 	la $a0,greeting_prompt
@@ -14,24 +15,81 @@ main:
 	
 #---------This is the loop---------#
 	#Creates Counter at $s1
-	li $s1, 0
+	li $s1, 1
 	#Creates Increment Variable
 	li $t1,1
+	
+	#----Stores Dividends----#
+	li $t3,3
+	li $t4,4
 	
 	start_loop:
 		move $t0,$s1
 		
 		#Prints Counter Integer		
-		li $v0,1
-		move $a0,$t0
-		syscall
+		#li $v0,1
+		#move $a0,$t0
+		#syscall
 		
-		#If counter equals user input jumps to end_loop
-		beq $s1,$s0,end_loop
+
 		
-		#If not equal adds 1 to counter and jumps to start_loop
-		add $s1,$s1,$t1
-		b start_loop
+		#---------This is IF BLOCK For Divisibility---------#	
+		start_divis3:
+			div $t0,$s1,$t3		#Divide by 3
+			mfhi $s2			#Saves Remained into $s2
+			beqz $s2,next_divis	#If Divisible By 3 then Check x/4
+		
+			#Not 3 but checking 4:
+			div  $s1,$t4
+			mfhi $s2
+			bgtz $s2,Print_Int	#If not divis then print int
+		
+			#divisible by 4 so print BABE and end
+			li $v0,4
+			la $a0,Babe_MSG
+			syscall
+		
+			#li $v0,4
+			#la $a0,End_MSG
+			#syscall
+		
+			b Finish
+		
+		#Check if divis by both 3 & 4
+		next_divis:
+			li $v0,4
+			la $a0,Feed_MSG
+			syscall
+		
+			div  $s1,$t4
+			mfhi $s2
+			bgtz $s2,Finish	#If not divis by 4 jumpt to Finish
+		
+			#It is divis by 4
+			li $v0,4
+			la $a0,Babe_MSG
+			syscall
+		
+			b Finish
+		
+		Finish:
+			li $v0, 4
+			la $a0, End_MSG
+			syscall
+			b closeOut
+		
+		Print_Int:
+			li $v0, 1
+			move $a0, $s1
+			syscall
+			b Finish
+		
+		closeOut:
+			#Restarting Loop#	
+			beq $s1,$s0,end_loop	#If counter equals user input jumps to end_loop
+			add $s1,$s1,$t1		#If not equal adds 1 to counter and jumps to start_loop
+			b start_loop		
+		
 	
 	end_loop:
 	li $v0,4
@@ -47,5 +105,9 @@ greeting_prompt:
 .asciiz "Please Enter a Number:"
 message:
 .asciiz "\nFinished Counting!"
-
-	
+Feed_MSG:
+	.asciiz "Feed"
+End_MSG:
+	.asciiz "\n"
+Babe_MSG:
+	.asciiz "Babe"
