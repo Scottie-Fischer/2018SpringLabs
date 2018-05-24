058abe8f86ebc9127681d871763cb26fcfbc1605
sifische <sifische@ucsc.edu>
2018-05-23 19:49:26 -0700

    Added Loop and Multiplication of Bits to Correctly Represent Decimal Value

--- lab5/Lab5.asm
+++ lab5/Lab5.asm
@@ -1,55 +1,63 @@
-#Scott Fischer
-#lab 5
-
-#----------Peudocode Outline----------#
-	#Read Input from Register
-	#Convert Argument into Binary Integer
-	#Store Binary Integer into $s0
-	
-	#Convert $s0 into ASCII decimal number:
-		#Loop Start: Loop 8 times (for each hex digit)
-			#Pass Through first two chars ("0x")
-			
-			#If Block that sets starting binary based on if digit or letter
-			
-			#If Block that sets value to be added if letter
-	
 .text
 main:
-	la $a1, 0x10010000	#Address of .data
+	la $a1, 0x10010000
+
+#-----Register Variables-----#
+	li $s3, 268435456
+	li $t0, 2			#Offset Counter for Address
+	addu $t1, $a1,$t0		#Holds Address of Current Char (3rd char) (Byte Address)
+	lb $t3, ($t1)			#Holds Actual Value of Char
 	
-	#Takes Address from $a1 and puts it in $s1
-	la $s1, ($a1)
-	#Echos String Given
+#-----Printing Prompt & Data-----#
 	li $v0,4
-	la $a0, ($s1)
+	la $a0, Prompt
+	syscall
+	li $v0,4
+	la $a0, ($a1)
+	syscall
+	li $v0,4
+	la $a0, New_Line
 	syscall
 	
-	#Testing Offset
-	la $t1, ($a1)
-	lb $t2, 2($t1)
-	
-	la $v0, 11			#11 is Print Char
-	la $a0, ($t2)
-	syscall
-	
-	#Testing For Adding Value With Offset
-	li $t3, 0		#$t3 holds offset amt (2)
-	addi $t3,$t3,1	#$t3 is now 3
-	addu $t4, $1,$t3	
-	lb $t5, ($t4)	#Loads the Byte of the offset address into $t5
-	
-	la $v0, 11		#11 is Print Char
-	la $a0, ($t5)	#Prints the value of $t5 (the value of offset address)
-	syscall
-	
-	#Exits cleanly
-	li $v0, 10
+	li $v0,11
+	la $a0,($t3)
 	syscall
 
+#----------IF Block----------#	#Determines if char is Digit or Letter
+	Start:
+	bgt $t3,0x40,Letter
+	#Digit:	
+	subi $t6,$t3,0x30			#If Digit subtracts 48
+	b Multi
+	
+	Letter:
+	subi $t6,$t3,0x37			#If Letter subtracts 55
+	b Multi
+	
+	Multi:
+	multu $t6,$s3
+	mflo $t7
+	addu $s0, $s0, $t7
+	b Split
+	
+	Split:
+	beq $t0,9,End
+	#Update Hex Number We Multiply By
+	divu $s3,$s3,16
+	#Updates Bit We Look At
+	addi $t0,$t0,1
+	addu $t1, $a1,$t0
+	lb $t3, ($t1)
+	b Start
+	
+	End:
+		li $v0,10
+		syscall
 
 .data
 String_Data:
 .asciiz "0xDEADBEEF"
-
-			
+Prompt:
+.asciiz "Input a hex number:\n"
+New_Line:
+.asciiz "\n"
