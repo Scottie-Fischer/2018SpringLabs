6c540a836198fdb219ecdbbe783d20a432f504e9
sifische <sifische@ucsc.edu>
2018-05-25 23:47:33 -0700

    Added Branch for 0x80000000 and Finished Print Loop

--- lab5/Lab5.asm
+++ lab5/Lab5.asm
@@ -10,8 +10,7 @@ main:
 	li $t0, 2				#Offset Counter for Address
 	addu $t1, $a1,$t0			#Holds Address of Current Char (3rd char) (Byte Address)
 	lb $t3, ($t1)			#Holds Actual Value of Char
-	
-#-----Printing Prompt & Data-----#
+#-----Printing Prompt & Program Argument-----#
 	li $v0,4
 	la $a0, Prompt
 	syscall
@@ -21,8 +20,7 @@ main:
 	li $v0,4
 	la $a0, New_Line
 	syscall
-
-#----------Loop----------#	#Determines if char is Digit or Letter
+#----------Turning ASCII Into Hex----------#
 	Loop_start:
 	bgt $t3,0x40,Letter
 	#Digit:	
@@ -41,64 +39,61 @@ main:
 	b Split
 	
 	Split:
-	beq $t0,9,To_ASCII		#If at end of String then end
-	
-	#Update Hex Number We Multiply By
-	divu $s3,$s3,16
+	beq $t0,9,To_Dec			#If at end of String then end
+	divu $s3,$s3,16			#Update Hex Number We Multiply By
 	
 	#Updates Hex Bit We Look At
 	addi $t0,$t0,1
 	addu $t1, $a1,$t0
 	lb $t3, ($t1)
 	b Loop_start			#Restarts Loop
-	
-	To_ASCII:
+#-----End Of ASCII LOOP----#
+#-----Start of Turning Binary to ASCII-----#
+	To_Dec:
 	li $t0, 0				#Counter for How Many Decimal Digits
-	andi $t7,$s0,0x80000000		#Repurposes $t7 to hold the first bit of binary number
-	srl $t7, $t7,31			
-	blt $t7,1,Positive		#Skips adding '-' char to array if positive
-	
-	li $t2,0x2D
-	sb $t2,myArray($t0)
-	lb $t4,myArray($zero)
-	addiu $t0,$t0,1
-	
 	#Sets $t7 to binary version of decimal number by converting to 2SC
-	Positive:
 	move $t7,$s0
+	bgtz $t7,bin2Dec			#Skips Converting Number to Positive Version
 	xori $t7,$t7 0xFFFFFFFF
-	addi $t7,$t7,1
+	addiu $t7,$t7,1
 	
 	bin2Dec:				#Loop converts binary $t7 to decimal number
-	beqz $t7,End			#Branches if no more Digits
+	beqz $t7,addSign			#Branches if no more Digits
 	rem $t6,$t7,10
 	mflo $t7
+	bltz $t6,hardCode
+	pos:
 	addi $t6,$t6,48
 	sb $t6,myArray($t0)
 	addi $t0,$t0,1
 	b bin2Dec
 	
+	addSign:				#Adds Negative Sign in Case Decimal Number is Negative
+	andi $t7,$s0,0x80000000		#Repurposes $t7 to hold the first bit of binary number
+	srl $t7, $t7,31			
+	blt $t7,1,End			#Skips adding '-' char to array if positive
+	li $t2,0x2D
+	sb $t2,myArray($t0)
+	lb $t4,myArray($zero)
+	addiu $t0,$t0,1
+	b End
+	hardCode:				#Multiplies Division by -1 to Ensure Correct ASCII Char
+	li $t5,-1
+	mult $t6,$t5
+	mflo $t6
+	b pos					#Continues Back to Process
+#-----Start of Printing Process-----#
 	End:
-		#Prints Integer
-		li $v0, 1
-		la $a0, ($s0)
-		syscall
-		li $v0,4
-		la $a0,New_Line
-		syscall
-		
-		PrintArray:
-		bltz $t0,Close
-		lb $t4,myArray($t0)
-		li $v0,11
-		la $a0,($t4)
-		syscall
-		subiu $t0,$t0,1
-		b PrintArray
-		
-		Close:
-		li $v0,10
-		syscall
+	bltz $t0,Close
+	lb $t4,myArray($t0)
+	li $v0,11
+	la $a0,($t4)
+	syscall
+	subiu $t0,$t0,1
+	b End
+	Close:
+	li $v0,10
+	syscall
 .data
 Prompt:
 .asciiz "Input a hex number:\n"
