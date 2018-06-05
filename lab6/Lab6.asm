22e03f2f34df9c2b2a6ca263a7433e95fed7f7e9
sifische <sifische@ucsc.edu>
2018-06-04 19:03:34 -0700

    Added CompareFloats Function, Proper Exiting of Functions, & Testing Structures

--- lab6/Lab6.asm
+++ lab6/Lab6.asm
@@ -7,9 +7,35 @@
 #input:		$a0 = Single Precision float
 #Side Effects:	None
 .text
+main:
+	li $a0,0x44800000
+	li $a1,0x3F000000
+	jal CompareFloats
+	move $t1,$v0
+	li $v0,1
+	la $a0,($t1)
+	syscall
+	li $a0,0x44800000
+	li $a1,0x44800000
+	jal CompareFloats
+	move $t1,$v0
+	li $v0,1
+	la $a0,($t1)
+	syscall
+	li $a0,0x3F000000
+	li $a1,0x44800000
+	jal CompareFloats
+	move $t1,$v0
+	li $v0,1
+	la $a0,($t1)
+	syscall
+	
+	li $v0,10
+	syscall
+
+.text
 PrintFloat:
-	#li $a0,0x44801000
-	li $a0,0xBEDCFFFF
+
 	move $t7,$a0
 	#-----Saving Registers-----#
 	subi $sp,$sp,48
@@ -93,8 +119,7 @@ PrintFloat:
 	lw $s7,44($sp)
 	addi $sp,$sp,48
 	#-----Closing-----#
-	li $v0,10
-	syscall
+	jr $ra
 
 .data
 Output_Msg:		.asciiz "Output:\n"
@@ -102,3 +127,53 @@ Sign_Msg:		.asciiz "Sign: "
 Exponent_Msg:	.asciiz "Exponent: "
 Mantissa_Msg:	.asciiz "Mantissa: "
 PNL:			.asciiz "\n"
+
+#----------Compare Floats Subroutine----------#
+#Subroutine: 	CompareFloats
+#Purpose:		Compares two floating precision A and B.
+#input:		$a0 = Single Precision float A
+#			$a1 = Single Precision float B
+#output:		$v0 = Comparison result
+#Side Effects:	None
+.text
+CompareFloats:
+#-----Saving Registers-----#
+	subi $sp,$sp,48
+	sw $a0,0($sp)
+	sw $a1,4($sp)
+	sw $a2,8($sp)
+	sw $a3,12($sp)
+	sw $s0,16($sp)
+	sw $s1,20($sp)
+	sw $s2,24($sp)
+	sw $s3,28($sp)
+	sw $s4,32($sp)
+	sw $s5,36($sp)
+	sw $s6,40($sp)
+	sw $s7,44($sp)
+#-----Comparing Floats-----#
+	bgt $a1,$a0,Greater
+	beq $a1,$a0,Equal
+	li $v0,1
+	b RestoreRegisters
+	Greater:
+		li $v0,-1
+		b RestoreRegisters
+	Equal:
+		li $v0,0
+#-----Restoring Registers-----#
+	RestoreRegisters:
+		lw $a0,0($sp)
+		lw $a1,4($sp)
+		lw $a2,8($sp)
+		lw $a3,12($sp)
+		lw $s0,16($sp)
+		lw $s1,20($sp)
+		lw $s2,24($sp)
+		lw $s3,28($sp)
+		lw $s4,32($sp)
+		lw $s5,36($sp)
+		lw $s6,40($sp)
+		lw $s7,44($sp)
+		addi $sp,$sp,48
+	jr $ra
